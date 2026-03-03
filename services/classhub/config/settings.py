fee8639a1057d005f35782a14235968eccdc8a12
Ben Severns <severns3@gmail.com>
2026-03-03 12:21:30 -0600

    Restore i18n context processor for template language code

--- services/classhub/config/settings.py
+++ services/classhub/config/settings.py
@@ -159,6 +159,7 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
+                "django.template.context_processors.i18n",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "config.context_processors.operator_profile",
